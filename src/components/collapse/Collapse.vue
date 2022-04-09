2b328d7ea3f39a91f7584a376670e299f61a2a0c
zach balder <zmbalder@gmail.com>
2022-04-09 18:53:58 -0400

    add collapse

---
+++ src/components/collapse/Collapse.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    tabindex="0"
+    class="collapse collapse-arrow border border-base-300 bg-white rounded-box">
+    <input type="checkbox">
+    <div class="collapse-title text-xl font-medium">
+      {{ title }}
+    </div>
+    <div class="collapse-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Collapse',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
